b3cff8bba0ec2f25958aa73dd5410e63557a37d5
Diego de Mello <diegodemello027@gmail.com>
2025-03-08 12:22:57 -0300

    Criando um componente e estilizando.

---
+++ src/components/DinamicTitle.js
@@ -0,0 +1,24 @@
+class DinamicTitle extends HTMLElement{
+    constructor(){
+        super();
+
+        const shadow = this.attachShadow({
+            mode: 'open'
+        });
+
+        const componentRoot = document.createElement('h1');
+        componentRoot.textContent = 'Diego';
+
+        const style = document.createElement('style');
+        style.textContent = `
+        h1{
+            color: red;
+        }
+        `;
+
+        shadow.appendChild(componentRoot);
+        shadow.appendChild(style);
+    }
+}
+
+customElements.define('dinamic-title', DinamicTitle);
